d39d5521d71976dde39beafc5e38066f463b6e4c
Viraj Nyayadhish <nviraj16@gmail.com>
2023-10-04 19:56:20 +0530

    Recreated with rainbow wallet and added basic layout

--- src/app/page.js
+++
@@ -1,9 +0,0 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import CourseForm from './courseForm'
-
-export default function Home() {
-  return (
-    <CourseForm />
-  )
-}
